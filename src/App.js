6dc4d3251b0b1cca9938bd5395d12f1e1d5ea6e9
A David <aimardavidninor@hotmail.com>
2022-10-29 15:52:32 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -22,8 +22,14 @@ function App() {
 
   return (
     <>
-    <div className = "container mt-5 mb-5">
-      <h3 className="text-center"> Api de Dragon ball</h3>
+    <div className = "container">
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container d-flex d-flex justify-content-start ">
+          <img src="https://e7.pngegg.com/pngimages/181/758/png-clipart-goku-dragon-ball-z-collectible-card-game-dragon-ball-collectible-card-game-logo-kartikeya-television-text.png" alt="" style={{maxWidth: "100px", maxHeight: "50px",}}/> 
+          <h3 className="ml-5 text-light" col> Api de Dragon ball</h3>
+        </div>
+        </nav>
+      
       < Personaje personajes={personajes} />
     </div>
     </>
